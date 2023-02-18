39632be18dc987dea6681cd3657891aa702b754f
JacobBartlett24 <jacobart@ttu.edu>
2023-02-18 14:55:17 -0600

    initial commit

---
+++ target.py
@@ -0,0 +1,102 @@
+# curl "https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&channel=WEB&count=24&default_purchasability_filter=true&include_sponsored=true&keyword=vinyl+records&offset=0&page=^%^2Fs^%^2Fvinyl+records&platform=mobile&pricing_store_id=83&sort_by=Featured&store_ids=83^%^2C2190^%^2C770&useragent=Mozilla^%^2F5.0+^%^28Linux^%^3B+Android+6.0^%^3B+Nexus+5+Build^%^2FMRA58N^%^29+AppleWebKit^%^2F537.36+^%^28KHTML^%^2C+like+Gecko^%^29+Chrome^%^2F110.0.0.0+Mobile+Safari^%^2F537.36&visitor_id=0186612F72AD020192513D4B25087EDC&zip=79364" ^
+#   -H "authority: redsky.target.com" ^
+#   -H "accept: application/json" ^
+#   -H "accept-language: en-US,en;q=0.9" ^
+#   -H "cookie: TealeafAkaSid=mh7cYCXaQAo5_tQqQAzb89Hydi38el3Q; visitorId=0186612F72AD020192513D4B25087EDC; sapphire=1; UserLocation=79364^|33.440^|-101.650^|TX^|US; accessToken=eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwMWU1NjFhZi1hNThjLTRkYWItODdjMS04YTkwMDIxMGZjY2YiLCJpc3MiOiJNSTYiLCJleHAiOjE2NzY3NTQxNDksImlhdCI6MTY3NjY2Nzc0OSwianRpIjoiVEdULmNhNWMyNGUzOWQ2NjRkYTg5NzEyOWIwODM3ZjM3NzYwLWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsImRpZCI6ImM0Njg3NzIwMDY4ZmVmODBiNjZjZDk0NDIzZTEzZTA4NTk2N2JkYzUxOTI1NmQ2OGM4MTk3OGRhNzUzNDcwMTMiLCJzY28iOiJlY29tLm5vbmUsb3BlbmlkIiwiY2xpIjoiZWNvbS13ZWItMS4wLjAiLCJhc2wiOiJMIn0.lgs9eGS3vEX_YMc-I_EsJbgjk1dPcHHViOA-hLzNaAXAuA41kKWBMxPVPp34WFljVksL5t8KCQUMR5sefrTRkIpAdXmGtfMfucU86cV2d-MZD8q7vCx17XgE5MqJOGpO4UjzyL3nx8TZJp-awVQzRYwxOUKX2lrmU8SUIb7ONjBfUNyPxBsd1K9wwd3c8jrC7OT7_U8XerRCNKiM6EuvIk1nzemnIW_QIdHt4V7KkC2IH_1K3ITMmWbTsBmVWku8NTvD57IPqfOHULhI8RD3SyiaumB9qUb0SKMkqUbpMBuIUaHSu8X7qveD1PtO-jZMWUWCmjqGjmx8k4csRpNGQg; idToken=eyJhbGciOiJub25lIn0.eyJzdWIiOiIwMWU1NjFhZi1hNThjLTRkYWItODdjMS04YTkwMDIxMGZjY2YiLCJpc3MiOiJNSTYiLCJleHAiOjE2NzY3NTQxNDksImlhdCI6MTY3NjY2Nzc0OSwiYXNzIjoiTCIsInN1dCI6IkciLCJjbGkiOiJlY29tLXdlYi0xLjAuMCIsInBybyI6eyJmbiI6bnVsbCwiZW0iOm51bGwsInBoIjpmYWxzZSwibGVkIjpudWxsLCJsdHkiOmZhbHNlfX0.; refreshToken=ax40mMv4Zfnb7IzNUCsynjH2V1sLMTmj1HxaOtqG9nKK3ExX_b5F4n8zELmMPm-VGTBV6d33Vat6en-KRBYOSg; fiatsCookie=DSI_83^|DSN_Lubbock^%^20University^%^20Ave^|DSZ_79423; ci_pixmgr=other; _gcl_au=1.1.538189206.1676667753; _ga=GA1.2.1619494967.1676668624; _gid=GA1.2.1291468420.1676668624; __gads=ID=745146e152674252:T=1676750262:S=ALNI_MazrU5IZnHEVSWlbvC_m-K10KC25g; __gpi=UID=0000094a135050e5:T=1676750262:RT=1676750262:S=ALNI_MZWX-O5SR8Z59c8VgHNHpd6TJ2geQ; _mitata=ZDg2Y2RlOTA0ZmJhYWNiY2U1NTdmNzhmNzM0OTIxNzYyMmMyYjVmODFjOTJiMjkyNGIyNGE2NTgxZjlhNDA5Zg==_/^@^#/1676750424_/^@^#/ctnXdTClgORooqSD_/^@^#/MzkxYmJlNDJkNzA5OTc3NGFiOGYwMWFhYTUxNjY2YTIxM2Y1NjE1OTk5ZjBiMzRhYTU3MzA5MWI0Mjg0OWRjNw==_/^@^#/000; ffsession=^{^%^22sessionHash^%^22:^%^229316a55e22a721676667747678^%^22^%^2C^%^22prevPageName^%^22:^%^22search:^%^20search^%^20results^%^22^%^2C^%^22prevPageType^%^22:^%^22search:^%^20search^%^20results^%^22^%^2C^%^22prevPageUrl^%^22:^%^22https://www.target.com/s?searchTerm=vinyl+records&tref=typeahead^%^257Cterm^%^257C0^%^257Cvinyl+records^%^257Cvinyl+records^%^257C^%^257C^%^257Cservice^%^257C10^%^257C^%^257C^%^257C&category=0^%^257CAll^%^257Cmatchallpartial^%^257Call+categories&searchTermRaw=&sortBy=Featured&moveTo=product-list-grid^%^22^%^2C^%^22prevSearchTerm^%^22:^%^22vinyl^%^20records^%^22^%^2C^%^22sessionHit^%^22:23^}; _uetsid=65a1dad0af0611ed8f463541a41dd873; _uetvid=65a1f630af0611edafaeeb821e37aa76" ^
+#   -H "origin: https://www.target.com" ^
+#   -H "referer: https://www.target.com/s?searchTerm=vinyl+records&tref=typeahead^%^7Cterm^%^7C0^%^7Cvinyl+records^%^7Cvinyl+records^%^7C^%^7C^%^7Cservice^%^7C10^%^7C^%^7C^%^7C&category=0^%^7CAll^%^7Cmatchallpartial^%^7Call+categories&searchTermRaw=&sortBy=Featured&moveTo=product-list-grid" ^
+#   -H "sec-ch-ua: ^\^"Chromium^\^";v=^\^"110^\^", ^\^"Not A(Brand^\^";v=^\^"24^\^", ^\^"Google Chrome^\^";v=^\^"110^\^"" ^
+#   -H "sec-ch-ua-mobile: ?1" ^
+#   -H "sec-ch-ua-platform: ^\^"Android^\^"" ^
+#   -H "sec-fetch-dest: empty" ^
+#   -H "sec-fetch-mode: cors" ^
+#   -H "sec-fetch-site: same-site" ^
+#   -H "user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36" ^
+#   --compressed
+
+import requests
+import json
+
+offset = 0
+
+cookies = {
+    'TealeafAkaSid': 'mh7cYCXaQAo5_tQqQAzb89Hydi38el3Q',
+    'visitorId': '0186612F72AD020192513D4B25087EDC',
+    'sapphire': '1',
+    'UserLocation': '79364|33.440|-101.650|TX|US',
+    'accessToken': 'eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwMWU1NjFhZi1hNThjLTRkYWItODdjMS04YTkwMDIxMGZjY2YiLCJpc3MiOiJNSTYiLCJleHAiOjE2NzY3NTQxNDksImlhdCI6MTY3NjY2Nzc0OSwianRpIjoiVEdULmNhNWMyNGUzOWQ2NjRkYTg5NzEyOWIwODM3ZjM3NzYwLWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsImRpZCI6ImM0Njg3NzIwMDY4ZmVmODBiNjZjZDk0NDIzZTEzZTA4NTk2N2JkYzUxOTI1NmQ2OGM4MTk3OGRhNzUzNDcwMTMiLCJzY28iOiJlY29tLm5vbmUsb3BlbmlkIiwiY2xpIjoiZWNvbS13ZWItMS4wLjAiLCJhc2wiOiJMIn0.lgs9eGS3vEX_YMc-I_EsJbgjk1dPcHHViOA-hLzNaAXAuA41kKWBMxPVPp34WFljVksL5t8KCQUMR5sefrTRkIpAdXmGtfMfucU86cV2d-MZD8q7vCx17XgE5MqJOGpO4UjzyL3nx8TZJp-awVQzRYwxOUKX2lrmU8SUIb7ONjBfUNyPxBsd1K9wwd3c8jrC7OT7_U8XerRCNKiM6EuvIk1nzemnIW_QIdHt4V7KkC2IH_1K3ITMmWbTsBmVWku8NTvD57IPqfOHULhI8RD3SyiaumB9qUb0SKMkqUbpMBuIUaHSu8X7qveD1PtO-jZMWUWCmjqGjmx8k4csRpNGQg',
+    'idToken': 'eyJhbGciOiJub25lIn0.eyJzdWIiOiIwMWU1NjFhZi1hNThjLTRkYWItODdjMS04YTkwMDIxMGZjY2YiLCJpc3MiOiJNSTYiLCJleHAiOjE2NzY3NTQxNDksImlhdCI6MTY3NjY2Nzc0OSwiYXNzIjoiTCIsInN1dCI6IkciLCJjbGkiOiJlY29tLXdlYi0xLjAuMCIsInBybyI6eyJmbiI6bnVsbCwiZW0iOm51bGwsInBoIjpmYWxzZSwibGVkIjpudWxsLCJsdHkiOmZhbHNlfX0.',
+    'refreshToken': 'ax40mMv4Zfnb7IzNUCsynjH2V1sLMTmj1HxaOtqG9nKK3ExX_b5F4n8zELmMPm-VGTBV6d33Vat6en-KRBYOSg',
+    'fiatsCookie': 'DSI_83|DSN_Lubbock%20University%20Ave|DSZ_79423',
+    'ci_pixmgr': 'other',
+    '_gcl_au': '1.1.538189206.1676667753',
+    '_ga': 'GA1.2.1619494967.1676668624',
+    '_gid': 'GA1.2.1291468420.1676668624',
+    '__gads': 'ID=745146e152674252:T=1676750262:S=ALNI_MazrU5IZnHEVSWlbvC_m-K10KC25g',
+    '__gpi': 'UID=0000094a135050e5:T=1676750262:RT=1676750262:S=ALNI_MZWX-O5SR8Z59c8VgHNHpd6TJ2geQ',
+    '_mitata': 'ZDg2Y2RlOTA0ZmJhYWNiY2U1NTdmNzhmNzM0OTIxNzYyMmMyYjVmODFjOTJiMjkyNGIyNGE2NTgxZjlhNDA5Zg==_/@#/1676750424_/@#/ctnXdTClgORooqSD_/@#/MzkxYmJlNDJkNzA5OTc3NGFiOGYwMWFhYTUxNjY2YTIxM2Y1NjE1OTk5ZjBiMzRhYTU3MzA5MWI0Mjg0OWRjNw==_/@#/000',
+    'ffsession': '{%22sessionHash%22:%229316a55e22a721676667747678%22%2C%22prevPageName%22:%22search:%20search%20results%22%2C%22prevPageType%22:%22search:%20search%20results%22%2C%22prevPageUrl%22:%22https://www.target.com/s?searchTerm=vinyl+records&tref=typeahead%257Cterm%257C0%257Cvinyl+records%257Cvinyl+records%257C%257C%257Cservice%257C10%257C%257C%257C&category=0%257CAll%257Cmatchallpartial%257Call+categories&searchTermRaw=&sortBy=Featured&moveTo=product-list-grid%22%2C%22prevSearchTerm%22:%22vinyl%20records%22%2C%22sessionHit%22:23}',
+    '_uetsid': '65a1dad0af0611ed8f463541a41dd873',
+    '_uetvid': '65a1f630af0611edafaeeb821e37aa76',
+}
+
+headers = {
+    'authority': 'redsky.target.com',
+    'accept': 'application/json',
+    'accept-language': 'en-US,en;q=0.9',
+    # 'cookie': 'TealeafAkaSid=mh7cYCXaQAo5_tQqQAzb89Hydi38el3Q; visitorId=0186612F72AD020192513D4B25087EDC; sapphire=1; UserLocation=79364|33.440|-101.650|TX|US; accessToken=eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwMWU1NjFhZi1hNThjLTRkYWItODdjMS04YTkwMDIxMGZjY2YiLCJpc3MiOiJNSTYiLCJleHAiOjE2NzY3NTQxNDksImlhdCI6MTY3NjY2Nzc0OSwianRpIjoiVEdULmNhNWMyNGUzOWQ2NjRkYTg5NzEyOWIwODM3ZjM3NzYwLWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsImRpZCI6ImM0Njg3NzIwMDY4ZmVmODBiNjZjZDk0NDIzZTEzZTA4NTk2N2JkYzUxOTI1NmQ2OGM4MTk3OGRhNzUzNDcwMTMiLCJzY28iOiJlY29tLm5vbmUsb3BlbmlkIiwiY2xpIjoiZWNvbS13ZWItMS4wLjAiLCJhc2wiOiJMIn0.lgs9eGS3vEX_YMc-I_EsJbgjk1dPcHHViOA-hLzNaAXAuA41kKWBMxPVPp34WFljVksL5t8KCQUMR5sefrTRkIpAdXmGtfMfucU86cV2d-MZD8q7vCx17XgE5MqJOGpO4UjzyL3nx8TZJp-awVQzRYwxOUKX2lrmU8SUIb7ONjBfUNyPxBsd1K9wwd3c8jrC7OT7_U8XerRCNKiM6EuvIk1nzemnIW_QIdHt4V7KkC2IH_1K3ITMmWbTsBmVWku8NTvD57IPqfOHULhI8RD3SyiaumB9qUb0SKMkqUbpMBuIUaHSu8X7qveD1PtO-jZMWUWCmjqGjmx8k4csRpNGQg; idToken=eyJhbGciOiJub25lIn0.eyJzdWIiOiIwMWU1NjFhZi1hNThjLTRkYWItODdjMS04YTkwMDIxMGZjY2YiLCJpc3MiOiJNSTYiLCJleHAiOjE2NzY3NTQxNDksImlhdCI6MTY3NjY2Nzc0OSwiYXNzIjoiTCIsInN1dCI6IkciLCJjbGkiOiJlY29tLXdlYi0xLjAuMCIsInBybyI6eyJmbiI6bnVsbCwiZW0iOm51bGwsInBoIjpmYWxzZSwibGVkIjpudWxsLCJsdHkiOmZhbHNlfX0.; refreshToken=ax40mMv4Zfnb7IzNUCsynjH2V1sLMTmj1HxaOtqG9nKK3ExX_b5F4n8zELmMPm-VGTBV6d33Vat6en-KRBYOSg; fiatsCookie=DSI_83|DSN_Lubbock%20University%20Ave|DSZ_79423; ci_pixmgr=other; _gcl_au=1.1.538189206.1676667753; _ga=GA1.2.1619494967.1676668624; _gid=GA1.2.1291468420.1676668624; __gads=ID=745146e152674252:T=1676750262:S=ALNI_MazrU5IZnHEVSWlbvC_m-K10KC25g; __gpi=UID=0000094a135050e5:T=1676750262:RT=1676750262:S=ALNI_MZWX-O5SR8Z59c8VgHNHpd6TJ2geQ; _mitata=ZDg2Y2RlOTA0ZmJhYWNiY2U1NTdmNzhmNzM0OTIxNzYyMmMyYjVmODFjOTJiMjkyNGIyNGE2NTgxZjlhNDA5Zg==_/@#/1676750424_/@#/ctnXdTClgORooqSD_/@#/MzkxYmJlNDJkNzA5OTc3NGFiOGYwMWFhYTUxNjY2YTIxM2Y1NjE1OTk5ZjBiMzRhYTU3MzA5MWI0Mjg0OWRjNw==_/@#/000; ffsession={%22sessionHash%22:%229316a55e22a721676667747678%22%2C%22prevPageName%22:%22search:%20search%20results%22%2C%22prevPageType%22:%22search:%20search%20results%22%2C%22prevPageUrl%22:%22https://www.target.com/s?searchTerm=vinyl+records&tref=typeahead%257Cterm%257C0%257Cvinyl+records%257Cvinyl+records%257C%257C%257Cservice%257C10%257C%257C%257C&category=0%257CAll%257Cmatchallpartial%257Call+categories&searchTermRaw=&sortBy=Featured&moveTo=product-list-grid%22%2C%22prevSearchTerm%22:%22vinyl%20records%22%2C%22sessionHit%22:23}; _uetsid=65a1dad0af0611ed8f463541a41dd873; _uetvid=65a1f630af0611edafaeeb821e37aa76',
+    'origin': 'https://www.target.com',
+    'referer': 'https://www.target.com/s?searchTerm=vinyl+records&tref=typeahead%7Cterm%7C0%7Cvinyl+records%7Cvinyl+records%7C%7C%7Cservice%7C10%7C%7C%7C&category=0%7CAll%7Cmatchallpartial%7Call+categories&searchTermRaw=&sortBy=Featured&moveTo=product-list-grid',
+    'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36',
+}
+
+params = {
+    'key': '9f36aeafbe60771e321a7cc95a78140772ab3e96',
+    'channel': 'WEB',
+    'count': '24',
+    'default_purchasability_filter': 'true',
+    'include_sponsored': 'true',
+    'keyword': 'vinyl',
+    'offset': f'{offset}',
+    'page': '/s/vinyl',
+    'platform': 'mobile',
+    'pricing_store_id': '83',
+    'sort_by': 'Featured',
+    'store_ids': '83,2190,770',
+    'useragent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36',
+    'visitor_id': '0186612F72AD020192513D4B25087EDC',
+    'zip': '79364',
+}
+
+def requestTarget():
+  response = requests.get(
+    'https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2',
+    params=params,
+    cookies=cookies,
+    headers=headers,
+  ) 
+  formatAsJson(response)
+
+def formatAsJson(response):
+  json_formatted_str = json.loads(response.text)
+
+  turnJSONintoReadable(json_formatted_str)
+
+  return json_formatted_str
+
+def turnJSONintoReadable(json_formatted_str):
+  products = json_formatted_str['data']['search']['products']
+  for product in products:
+    print(product['item']['product_description']['title'])
+
+for i in range(0, 100, 1):
+  requestTarget()
+  offset += 24
+  print('-------------------------------')
+  print(f'Offset: {offset}')
